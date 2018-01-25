3c8f75c5b455c1f0590ad82f1b194e8b6fe72851
MSI\Laurens <laurens@integrationpartners.nl>
2018-01-25 15:57:56 +0100

    Change test method to iterational

--- example/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestIT.java
+++ example/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestIT.java
@@ -1,5 +1,10 @@
 package nl.nn.adapterframework.webcontrol.api;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -7,19 +12,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ChromeTestIT extends ChromeTestBase {
+	public static Map<String, String> navigations = new HashMap<String, String>();
 
+	public ChromeTestIT() throws Exception {
+		navigations.put("Configurations", "#side-menu li:nth-child(3) a");
+		navigations.put("Logging", "#side-menu li:nth-child(4) a");
+	}
+	
 	@Test
 	public void PageTitle() throws Exception {
 		Assert.assertEquals("IAF | Adapter Status", driver.getTitle());
 	}
 	
 	@Test
-	public void NavigateToConfiguration() throws Exception {
-		String selector = "#side-menu li:nth-child(3) a";
+	public void Navigation() throws Exception {
+		Iterator<Entry<String, String>> it = navigations.entrySet().iterator();
 		
-		waitUntilVisible(By.cssSelector(selector));
-		driver.findElement(By.cssSelector(selector)).click();
-
-		Assert.assertEquals("IAF | Configurations", driver.getTitle());
+		while(it.hasNext()){
+			Map.Entry pair = (Map.Entry)it.next();
+			waitAndClick( By.cssSelector( (String) pair.getValue() ) );
+			Assert.assertEquals("IAF | " + pair.getKey(), driver.getTitle() );
+			it.remove();
+		}
+//		waitAndClick(By.cssSelector(navConfigurationSelector));
+//		Assert.assertEquals("IAF | Configurations", driver.getTitle() );
+//		
+//		waitAndClick(By.cssSelector(navLoggingSelector));
+//		Assert.assertEquals("IAF | Logging", driver.getTitle() );
 	}
-}
+}	
