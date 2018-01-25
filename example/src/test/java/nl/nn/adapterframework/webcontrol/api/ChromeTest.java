7fa521a6dcc32a2c58380cd7014d0ba0c4be5b9a
MSI\Laurens <laurens@integrationpartners.nl>
2018-01-25 13:55:09 +0100

    Test pre and post unit tests

--- example/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTest.java
+++ example/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ChromeTestIT extends ChromeTestBase {
+public class ChromeTest extends ChromeTestBase {
 
 	@Test
 	public void PageTitle() throws Exception {
